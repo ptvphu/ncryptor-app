735a906f227868c20104a8c4ea7c532d8b62ad12
Jonathan Boutsicaris <jboutsicaris@gmail.com>
2025-04-26 12:53:25 -0400

    52: bump ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.datalocker.ncryptor"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     aaptOptions {
         noCompress.add("so")
